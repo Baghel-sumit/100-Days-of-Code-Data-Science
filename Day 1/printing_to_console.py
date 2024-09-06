7f2bbc1bb7b9cd9698813cc3bc84b922257bf375
Sumit Baghel <sarvgt435216@gmail.com>
2024-09-07 00:12:41 +0530

    started day 1 of 100 days of python code

---
+++ Day 1/printing_to_console.py
@@ -0,0 +1,7 @@
+print("hello world\nhow are you? bro.\nthe great thing i can do ever.")
+
+print("\twe are printing with using \rtabs")
+
+yourName = input("Please enter your name:")
+
+print("your name is : ", yourName)
