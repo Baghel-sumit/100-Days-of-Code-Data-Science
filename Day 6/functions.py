9110ced725b6f6099f738b7bd2d1827306cb08c4
Sumit Baghel <sarvgt435216@gmail.com>
2024-09-13 08:10:59 +0530

    started day 6 of 100 days of python programming

---
+++ Day 6/functions.py
@@ -0,0 +1,15 @@
+# We'll learn about functions
+
+print("Hello world")
+
+print(len("hello"))
+
+# Defining our own functions
+
+def myFunc():
+    print("hello world inside the function")    
+    return
+
+# Calling the functions
+
+myFunc()
