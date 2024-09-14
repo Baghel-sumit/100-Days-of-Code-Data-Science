a77e23d36d4da42b4c3ffcfe294910a49840190b
Sumit Baghel <sarvgt435216@gmail.com>
2024-09-14 08:43:26 +0530

    completed day 6

---
+++ Day 6/while_loops.py
@@ -0,0 +1,10 @@
+# we'll learn about the while loop
+
+number_of_hurdles = 6
+
+while(number_of_hurdles > 0):
+    print("jumped")
+    number_of_hurdles -= 1
+    
+    print("hurdle", number_of_hurdles)
+
